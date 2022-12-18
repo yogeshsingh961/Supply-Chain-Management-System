3397b2474c4de3e17ed58113d48b91c26f375d60
Yogesh Singh <yogeshsingh3596@gmail.com>
2022-12-18 13:26:43 +0530

    header bar

--- src/main/java/com/example/supplychainsystem/HelloApplication.java
+++ src/main/java/com/example/supplychainsystem/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.supplychainsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -12,6 +13,16 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import javafx.scene.layout.Pane;
 public class HelloApplication extends Application {
+    public static final int width=700,height=600,headerbar=50;
+    Pane bodyPane= new Pane();
+    private GridPane headerBar(){
+       TextField searchText= new TextField();
+       Button searchButton= new Button("Search");
+       GridPane gridPane= new GridPane();
+       gridPane.add(searchText,0,0);
+       gridPane.add(searchButton,1,0);
+       return gridPane;
+    }
     private GridPane loginPage(){
       Label emailLabel= new Label("Email");
       Label passwordLabel=new Label("Password");
@@ -27,7 +38,13 @@ public class HelloApplication extends Application {
     }
     private Pane createContent(){
         Pane root= new Pane();
-        root.getChildren().addAll(loginPage());
+        root.setPrefSize(width,height+headerbar);
+        bodyPane.setMinSize(width,height);
+        bodyPane.setTranslateY(headerbar);
+        bodyPane.getChildren().addAll(loginPage());
+
+        root.getChildren().addAll(headerBar(),bodyPane);
+
         return root;
     }
     @Override
