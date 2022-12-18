eab228d11775631ad015e8280df80f5e45fa43f6
Yogesh Singh <yogeshsingh3596@gmail.com>
2022-12-18 11:51:29 +0530

    First commit

---
+++ src/main/java/com/example/supplychainsystem/HelloApplication.java
@@ -0,0 +1,43 @@
+package com.example.supplychainsystem;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.scene.layout.Pane;
+public class HelloApplication extends Application {
+    private GridPane loginPage(){
+      Label emailLabel= new Label("Email");
+      Label passwordLabel=new Label("Password");
+      TextField emailTextField=new TextField();
+      PasswordField passwordField= new PasswordField();
+      GridPane gridPane= new GridPane();
+      gridPane.add(emailLabel,0,0);
+      gridPane.add(emailTextField,1,0);
+      gridPane.add(passwordLabel,0,1);
+      gridPane.add(passwordField,1,1);
+      return gridPane;
+    }
+    private Pane createContent(){
+        Pane root= new Pane();
+        return root;
+    }
+    @Override
+    public void start(Stage stage) throws IOException {
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(createContent());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.supplychainsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.supplychainsystem to javafx.fxml;
+    exports com.example.supplychainsystem;
+}
